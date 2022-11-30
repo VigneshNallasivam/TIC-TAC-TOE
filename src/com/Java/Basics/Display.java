cc7d7a2c9aad33e207b7a7d347596d934966e33f
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-30 13:05:17 +0530

    Added : Master - Printing the Welcome Message For TIC-TAC-TOE game

---
+++ src/com/Java/Basics/Display.java
@@ -0,0 +1,11 @@
+package com.Java.Basics;
+
+public class Display 
+{
+
+	public static void main(String[] args) 
+	{
+      System.out.println("Welcome To TIC - TAC - TOE Gaming Program");
+	}
+
+}
